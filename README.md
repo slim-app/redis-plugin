f7c2612071817b3badd43555416fd82fb3f4297d
Soeren <soeren@siegismund-poschmann.de>
2015-03-02 16:20:46 +0100

    Redit Plugin

---
+++ README.md
@@ -0,0 +1,5 @@
+Use:
+$redis = \SlimPlugin\Redis\Redis::getRedis();
+
+Than you can use $redis as Redis() instance with the Documantion on Github:
+https://github.com/phpredis/phpredis
---
+++ SlimPlugin/Redis/Redis.php
@@ -0,0 +1,21 @@
+<?php
+namespace SlimPlugin\Redis;
+class Redis
+{
+    public static function config($configJson)
+    {
+        if($configJson["redis"]["enabled"]==true)
+        {
+            self::$redis = new Redis();
+            self::$redis->connect($configJson["redis"]["host"], $configJson["redis"]["port"]);
+            if(isset($configJson["redis"]["auth"])&&$configJson["redis"]["auth"]!="")
+            {
+                self::$redis->auth($configJson["redis"]["auth"]);
+            }
+        }
+    }
+    public static function getRedis()
+    {
+        return self::$redis;
+    }
+}
