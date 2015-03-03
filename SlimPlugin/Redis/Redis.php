ced699fcb0638d092f38db5450e8f584900a2961
Soeren <soeren@siegismund-poschmann.de>
2015-03-03 11:17:35 +0100

    Fix Bug

--- SlimPlugin/Redis/Redis.php
+++ SlimPlugin/Redis/Redis.php
@@ -2,11 +2,12 @@
 namespace SlimPlugin\Redis;
 class Redis
 {
+    private static $redis;
     public static function config($configJson)
     {
         if($configJson["redis"]["enabled"]==true)
         {
-            self::$redis = new Redis();
+            self::$redis = new \Redis();
             self::$redis->connect($configJson["redis"]["host"], $configJson["redis"]["port"]);
             if(isset($configJson["redis"]["auth"])&&$configJson["redis"]["auth"]!="")
             {
